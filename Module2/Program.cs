c0c202638f5975d3e5770cd3ddeb7865440c0b7c
james.morgan <jem27786@email.vccs.edu>
2026-09-15 16:07:43 -0400

    commit Module3

---
+++ Module2/Program.cs
@@ -0,0 +1,4 @@
+﻿string patientName;
+Console.WriteLine("Who is being seen today?");
+patientName = Console.ReadLine();
+Console.WriteLine($"Welcome, {patientName}!");
---
+++ Module3/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Hello!");
+
+Console.WriteLine("What was the total for your oil change?");
+
+double oilChangeTotal = double.Parse(Console.ReadLine());
+
+Console.WriteLine("What was the total for your tires?");
+
+double tireTotal = double.Parse(Console.ReadLine());
+
+Console.WriteLine("What was the total for your inspection?");
+
+double inspectionTotal = double.Parse(Console.ReadLine());
+
+double total = oilChangeTotal + tireTotal + inspectionTotal;
+total = total + (total * 0.06); 
+Console.WriteLine($"Your subtotal is: ${total:F2}");         
+Console.WriteLine($"Your tax is: ${total - oilChangeTotal - tireTotal - inspectionTotal:F2}");
+Console.WriteLine($"Your total with tax is: ${total:F2}");
